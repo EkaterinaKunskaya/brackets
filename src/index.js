9d373166655bb6e8b2e21260b98d424bd7fda902
EkaterinaKunskaya <nuar_23@mail.ru>
2022-08-09 17:22:35 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+
+  let arrayOfBrackets = bracketsConfig.map(i => i.join(''));
+
+  for (let i = 0; i < arrayOfBrackets.length; i++) {
+    if (str.includes(arrayOfBrackets[i])) {
+      str = str.replace(arrayOfBrackets[i], '');
+      i = -1;
+    };
+  }
+
+  return !Boolean(str.length);
 }
